eceae0d0af10f0832f59aa72ed40953b29673122
Sourabh <sourabhsahu69733@gmail.com>
2026-09-12 23:54:18 +0530

    chore: package

---
+++ BendyFreeApp/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "BendyFree",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "BendyFree", targets: ["BendyFree"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "BendyFree",
+            path: "Sources/BendyFree"
+        )
+    ]
+)
